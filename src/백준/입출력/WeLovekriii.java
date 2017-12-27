6a436cd2c46f6e2fa0968c199dcc9a7fb9147c3f
dohun94 <dohun94@naver.com>
2017-12-27 18:39:44 +0900

    [추가] //10718번 : We love kriii

---
+++ src/백준/입출력/WeLovekriii.java
@@ -0,0 +1,11 @@
+package 백준.입출력;
+
+//10718번 : We love kriii
+
+public class WeLovekriii {
+	public static void main(String[] args) {
+		for(int i = 0; i < 2; i++) {
+			System.out.println("강한친구 대한육군");
+		}
+	}
+}
