b3eeebd8d589149f46a5bbee1264e025c804e346
dohun94 <dohun94@naver.com>
2018-01-06 23:32:37 +0900

    [수정] 선택정렬 추가

--- 백준/백준/정렬/Sort1.java
+++ 백준/백준/정렬/Sort1.java
@@ -22,21 +22,41 @@ public class Sort1 {
 		// sort() 메서드를 이용한 정렬
 		// Arrays.sort(arr);
 		
-		// 삽입 정렬
-		int target = 0;
-		int key = 0;
-		for (int i = 1; i < arr.length; i++) {
-			key = arr[i]; target = i - 1;
-			
-			while(target >= 0 && key < arr[target]) {
-				arr[target + 1] = arr[target];
-				target--;
+		// 선택 정렬
+		// 시간복잡도 O(n^2) 공간복잡도 O(n)
+		int minIndex = 0;
+		for(int i = 0; i < arr.length; i++) {
+			minIndex = i;
+			for(int j = i + 1; j < arr.length; j++) {
+				if(arr[minIndex] > arr[j]) {
+					minIndex = j;
+				}
 			}
-			arr[target + 1] = key;
+			swap(arr, i, minIndex);
 		}
+		
+		// 삽입 정렬
+		// 최악 시간복잡도 O(n^2) 공간복잡도 O(n)
+//		int target = 0;
+//		int key = 0;
+//		for (int i = 1; i < arr.length; i++) {
+//			key = arr[i]; target = i - 1;
+//			
+//			while(target >= 0 && key < arr[target]) {
+//				arr[target + 1] = arr[target];
+//				target--;
+//			}
+//			arr[target + 1] = key;
+//		}
 
 		// 출력
 		Arrays.stream(arr).forEach((n) -> System.out.println(n));
 
+	}
+
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 }
