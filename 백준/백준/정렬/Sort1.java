39d6343d940b9eaaf1aab854ba1280c06d0a86f6
dohun94 <dohun94@naver.com>
2018-01-07 21:18:42 +0900

    [수정] 버블정렬 추가

--- 백준/백준/정렬/Sort1.java
+++ 백준/백준/정렬/Sort1.java
@@ -21,33 +21,43 @@ public class Sort1 {
 
 		// sort() 메서드를 이용한 정렬
 		// Arrays.sort(arr);
-		
+
 		// 선택 정렬
 		// 시간복잡도 O(n^2) 공간복잡도 O(n)
-		int minIndex = 0;
-		for(int i = 0; i < arr.length; i++) {
-			minIndex = i;
-			for(int j = i + 1; j < arr.length; j++) {
-				if(arr[minIndex] > arr[j]) {
-					minIndex = j;
-				}
-			}
-			swap(arr, i, minIndex);
-		}
-		
+		// int minIndex = 0;
+		// for(int i = 0; i < arr.length; i++) {
+		// minIndex = i;
+		// for(int j = i + 1; j < arr.length; j++) {
+		// if(arr[minIndex] > arr[j]) {
+		// minIndex = j;
+		// }
+		// }
+		// swap(arr, i, minIndex);
+		// }
+
 		// 삽입 정렬
 		// 최악 시간복잡도 O(n^2) 공간복잡도 O(n)
-//		int target = 0;
-//		int key = 0;
-//		for (int i = 1; i < arr.length; i++) {
-//			key = arr[i]; target = i - 1;
-//			
-//			while(target >= 0 && key < arr[target]) {
-//				arr[target + 1] = arr[target];
-//				target--;
-//			}
-//			arr[target + 1] = key;
-//		}
+		// int target = 0;
+		// int key = 0;
+		// for (int i = 1; i < arr.length; i++) {
+		// key = arr[i]; target = i - 1;
+		//
+		// while(target >= 0 && key < arr[target]) {
+		// arr[target + 1] = arr[target];
+		// target--;
+		// }
+		// arr[target + 1] = key;
+		// }
+
+		// 버블 정렬
+		// 시간복잡도 O(n^2)
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 1; j < arr.length - i; j++) {
+				if (arr[j] < arr[j - 1]) {
+					swap(arr, j, j - 1);
+				}
+			}
+		}
 
 		// 출력
 		Arrays.stream(arr).forEach((n) -> System.out.println(n));
