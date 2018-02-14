f16e19743ca8c8e322c1022b23199fcfcb7b9e8d
dohun94 <dohun94@naver.com>
2018-02-14 18:16:11 +0900

    [추가] //11725번(트리의 부모 찾기)

--- 백준/백준/트리/FindParents.java
+++ 백준/백준/트리/FindParents.java
@@ -1,74 +1,71 @@
 package 백준.트리;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
-//11725번(트리의 부모 찾기, 시간초과)
+//11725번(트리의 부모 찾기)
 
 public class FindParents {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int tc = sc.nextInt();
-		
+
 		int[] parentArr = new int[tc + 1];
-		boolean[] hasParent = new boolean[tc + 1];
-		hasParent[1] = true;
-		
-		List<Pair> list = new LinkedList<>();
-		
-		for(int i = 1; i < tc; i++) {
+		parentArr[1] = 1;
+
+		Stack<Pair> stack1 = new Stack<Pair>();
+		Stack<Pair> stack2 = new Stack<Pair>();
+
+		for (int i = 1; i < tc; i++) {
 			int first = sc.nextInt();
 			int second = sc.nextInt();
-			
-			if(!hasParent[first] && !hasParent[second]) {
-				list.add(new Pair(first, second));
-				continue;
-			}
-			
-			if(hasParent[first]) {
+
+			if (parentArr[first] > 0) {
 				parentArr[second] = first;
-				hasParent[second] = true;
-			} else {
+			} else if (parentArr[second] > 0) {
 				parentArr[first] = second;
-				hasParent[first] = true;
+			} else {
+				stack1.push(new Pair(first, second));
 			}
 		}
-		
-		Iterator<Pair> iter = list.iterator();
-		
-		while(!list.isEmpty()) {
-			while(iter.hasNext()) {
-				Pair item = iter.next();
-				
-				if(!hasParent[item.first] && !hasParent[item.second]) {
-					continue;
-				}
-				
-				if(hasParent[item.first]) {
-					parentArr[item.second] = item.first;
-					hasParent[item.second] = true;
-				} else {
-					parentArr[item.first] = item.second;
-					hasParent[item.first] = true;
-				}
-				
-				iter.remove();
+
+		sc.close();
+
+		Stack<Pair> currentStack = null;
+		Stack<Pair> nextStack = null;
+
+		while (!stack1.isEmpty() || !stack2.isEmpty()) {
+			if (stack1.isEmpty()) {
+				currentStack = stack2;
+				nextStack = stack1;
+			} else if (stack2.isEmpty()) {
+				currentStack = stack1;
+				nextStack = stack2;
+			}
+
+			Pair top = currentStack.peek();
+
+			if (parentArr[top.first] > 0) {
+				parentArr[top.second] = top.first;
+				currentStack.pop();
+			} else if (parentArr[top.second] > 0) {
+				parentArr[top.first] = top.second;
+				currentStack.pop();
+			} else {
+				nextStack.push(currentStack.pop());
 			}
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 2; i < parentArr.length; i++) {
+
+		for (int i = 2; i < parentArr.length; i++) {
 			sb.append(parentArr[i]);
 			sb.append(System.lineSeparator());
 		}
-		
+
 		System.out.println(sb);
-		sc.close();
 	}
 
 }
@@ -76,7 +73,7 @@ public class FindParents {
 class Pair {
 	int first;
 	int second;
-	
+
 	public Pair(int first, int second) {
 		this.first = first;
 		this.second = second;
