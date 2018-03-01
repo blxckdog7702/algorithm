c872c80b05c31843fbf6756357d484a8d09e811a
dohun94 <dohun94@naver.com>
2018-03-01 18:38:54 +0900

    [수정] //2667번(단지번호붙이기)

--- 백준/백준/DFSBFS/SectorNumberingDrive.java
+++ 백준/백준/DFSBFS/SectorNumberingDrive.java
@@ -1,6 +1,10 @@
 package 백준.DFSBFS;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 //2667번(단지번호붙이기)
@@ -17,12 +21,9 @@ public class SectorNumberingDrive {
 		sn.init(n);
 		sn.input(sc);
 
-		System.out.println(Arrays.deepToString(sn.map));
-
 		sc.close();
 
-		int sectorCount = 0;
-
+		sn.bfs();
 	}
 
 }
@@ -32,6 +33,7 @@ class SectorNumbering {
 	int[][] map;
 	boolean[][] visit;
 	int move[][] = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+	List<Integer> houseNumberList;
 
 	public void input(Scanner sc) {
 		for (int i = 0; i < n; i++) {
@@ -47,9 +49,48 @@ class SectorNumbering {
 		this.n = n;
 		map = new int[n][n];
 		visit = new boolean[n][n];
+		houseNumberList = new ArrayList<>();
 	}
 
 	public void bfs() {
+		int sectorCount = 0;
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				if(map[i][j] == 1 && !visit[i][j]) {
+					int houseNum = 0;
+					Queue<int[]> q = new LinkedList<>();
+					q.add(new int[] {i , j});
+					visit[i][j] = true;
+					houseNum++;
+					
+					while(!q.isEmpty()) {
+						int[] currentPos = q.poll();
+						
+						for(int k = 0; k < move.length; k++) {
+							int[] nextPos = new int[] {currentPos[0] + move[k][0], currentPos[1] + move[k][1]};
+							
+							if(nextPos[0] >= 0 && nextPos[0] < n && nextPos[1] >= 0 && nextPos[1] < n && !visit[nextPos[0]][nextPos[1]] && map[nextPos[0]][nextPos[1]] == 1) {
+								q.add(nextPos);
+								visit[nextPos[0]][nextPos[1]] = true;
+								houseNum++;
+							}
+						}
+					}
+					
+					houseNumberList.add(houseNum);
+					sectorCount++;
+				}
+			}
+		}
+		
+		System.out.println(sectorCount);
+		
+		Collections.sort(houseNumberList);
+		
+		for(int i : houseNumberList) {
+			System.out.println(i);
+		}
 		
 	}
 
