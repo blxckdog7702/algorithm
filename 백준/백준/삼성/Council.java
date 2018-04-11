e69a835d2a5d7548622723fdc64163e2dfafdcea
dohun94 <dohun94@naver.com>
2018-04-11 15:57:54 +0900

    [추가] //14501번(퇴사)(실패)

---
+++ 백준/백준/삼성/Council.java
@@ -0,0 +1,53 @@
+package 백준.삼성;
+
+import java.util.Scanner;
+
+//14501번(퇴사)(실패)
+
+public class Council {
+
+	private static int n;
+	private static int[][] arr;
+	private static int max;
+	private static boolean[] visit;
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		n = in.nextInt();
+		
+		arr = new int[2][n];
+		
+		for(int i = 0; i < n; i++) {
+			arr[0][i] = in.nextInt();
+			arr[1][i] = in.nextInt();
+		}
+		in.close();
+		
+		visit = new boolean[n];
+		max = 0;
+		
+		for(int i = 0; i < n; i++) {
+			dfs(i, 0);	
+		}
+		
+		System.out.println(max);
+	}
+
+	private static void dfs(int idx, int sum) {
+		if(idx == n) {
+			max = Math.max(max, sum);
+			return;
+		}
+		
+		for(int i = idx; i < n; i++) {
+			if(i + arr[0][i] - 1 < n) {
+				dfs(i + arr[0][i], sum + arr[1][i]);
+			} else {
+				max = Math.max(max, sum);
+				return;
+			}
+		}
+	}
+
+}
