a2827332c7c474e755885ffa651aad118170b1a1
dohun94 <dohun94@naver.com>
2018-04-12 17:59:28 +0900

    [추가] //14890번(경사로)(실패)

---
+++ 백준/백준/삼성/SlopeWay.java
@@ -0,0 +1,185 @@
+package 백준.삼성;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//14890번(경사로)(실패)
+
+public class SlopeWay {
+	private static int n;
+	private static int l;
+	private static int[][] arr;
+	private static int count;
+	private static boolean[][] visit;
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		n = in.nextInt();
+		l = in.nextInt();
+
+		arr = new int[n][n];
+		visit = new boolean[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = in.nextInt();
+			}
+		}
+		
+		in.close();
+
+		count = 0;
+
+		checkHori(arr);
+		
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(visit[i], false);
+		}
+		
+		checkVerti(arr);
+
+		System.out.println(count);
+
+	}
+
+	private static void checkVerti(int[][] arr) {
+		for (int j = 0; j < n; j++) {
+			boolean check = true;
+
+			for (int i = 0; i < n - 1; i++) {
+				// 큰거 만났을 때
+				if (arr[i][j] < arr[i + 1][j]) {
+					if (arr[i + 1][j] - arr[i][j] == 2) {
+						check = false;
+						break;
+					}
+
+					if (i - (l - 1) < 0) {
+						check = false;
+						break;
+					}
+
+					for (int k = i; k >= i - (l - 1); k--) {
+						if (arr[i][j] != arr[k][j]) {
+							check = false;
+							break;
+						}
+
+						if (visit[k][j]) {
+							check = false;
+							break;
+						}
+
+						visit[k][j] = true;
+					}
+				}
+				// 작은거 만났을때
+				else if (arr[i][j] > arr[i + 1][j]) {
+					if (arr[i][j] - arr[i + 1][j] == 2) {
+						check = false;
+						break;
+					}
+
+					if (i + l >= n) {
+						check = false;
+						break;
+					}
+
+					for (int k = i + 1; k <= i + l; k++) {
+						if (arr[i + 1][j] != arr[k][j]) {
+							check = false;
+							break;
+						}
+
+						if (visit[k][j]) {
+							check = false;
+							break;
+						}
+
+						visit[k][j] = true;
+					}
+				} else {
+					continue;
+				}
+			}
+
+			if (check)
+
+			{
+//				System.out.println("세로 : " + j);
+				count++;
+			}
+
+		}
+	}
+
+	private static void checkHori(int[][] arr) {
+		for (int i = 0; i < n; i++) {
+			boolean check = true;
+
+			for (int j = 0; j < n - 1; j++) {
+				// 큰거 만났을 때
+				if (arr[i][j] < arr[i][j + 1]) {
+					if (arr[i][j + 1] - arr[i][j] == 2) {
+						check = false;
+						break;
+					}
+
+					if (j - (l - 1) < 0) {
+						check = false;
+						break;
+					}
+
+					for (int k = j; k >= j - (l - 1); k--) {
+						if (arr[i][j] != arr[i][k]) {
+							check = false;
+							break;
+						}
+
+						if (visit[i][k]) {
+							check = false;
+							break;
+						}
+
+						visit[i][k] = true;
+					}
+				}
+				// 작은거 만났을때
+				else if (arr[i][j] > arr[i][j + 1]) {
+					if (arr[i][j] - arr[i][j + 1] == 2) {
+						check = false;
+						break;
+					}
+
+					if (j + l >= n) {
+						check = false;
+						break;
+					}
+
+					for (int k = j + 1; k <= j + l; k++) {
+						if (arr[i][j + 1] != arr[i][k]) {
+							check = false;
+							break;
+						}
+
+						if (visit[i][k]) {
+							check = false;
+							break;
+						}
+
+						visit[i][k] = true;
+					}
+				} else {
+					continue;
+				}
+			}
+
+			if (check) {
+//				System.out.println("가로 : " + i);
+				count++;
+			}
+
+		}
+	}
+}
