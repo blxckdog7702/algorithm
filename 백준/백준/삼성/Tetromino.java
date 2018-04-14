f9be5aa763013d88ea19156a353e13065f6f55db
dohun94 <dohun94@naver.com>
2018-04-15 01:23:40 +0900

    [추가] //14500번(테트로미노)

--- 백준/백준/삼성/Tetromino.java
+++ 백준/백준/삼성/Tetromino.java
@@ -1,9 +1,8 @@
 package 백준.삼성;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-//14500��(��Ʈ�ι̳�)
+//14500번(테트로미노)
 
 public class Tetromino {
 	private static int n;
@@ -17,8 +16,8 @@ public class Tetromino {
 
 		n = in.nextInt();
 		m = in.nextInt();
-		
-		maxleng = Math.max(n, m);
+
+		maxleng = 500;
 
 		origin = new int[maxleng][maxleng];
 
@@ -31,74 +30,73 @@ public class Tetromino {
 
 		max = 0;
 
-//		for (int[] a : origin) {
-//			System.out.println(Arrays.toString(a));
-//		}
-//
-//		System.out.println();
-//		
-//		for(int i = 0; i < 4; i++) {
-//			turnToClockwise(origin);
-//			calc();
-//			turnToReverse(origin);
-//			calc();
-//			turnToReverse(origin);
-//		}
-		
-		turnToClockwise(origin);
-		
-		for (int[] a : origin) {
-		System.out.println(Arrays.toString(a));
-	}
-		
-		System.out.println();
-		
-		turnToClockwise(origin);
+		// for (int[] a : origin) {
+		// System.out.println(Arrays.toString(a));
+		// }
+		//
 
-		for (int[] a : origin) {
-		System.out.println(Arrays.toString(a));
-	}
-		
+		// int[][] result = turnToClockwise(origin);
+
+		// for (int[] a : origin) {
+		// System.out.println(Arrays.toString(a));
+		// }
+		//
+		// System.out.println();
+		//
+		// turnToClockwise(origin);
+		//
+		// for (int[] a : origin) {
+		// System.out.println(Arrays.toString(a));
+		// }
+
+		for (int i = 0; i < 4; i++) {
+			turnToClockwise(origin);
+			calc();
+			turnToReverse(origin);
+			calc();
+			turnToReverse(origin);
+		}
+
 		System.out.println(max);
 	}
 
 	public static void calc() {
-		for (int i = 0; i < n; i++) {
-			for( int j = 0; j < m; j++) {
-				if(j + 3 < m) {
-					int m1 = origin[i][j] + origin[i][j + 1] + origin[i][j + 2] +origin[i][j + 3];
-//					System.out.println(m1);
+		for (int i = 0; i < maxleng; i++) {
+			for (int j = 0; j < maxleng; j++) {
+				if (j + 3 < maxleng) {
+					int m1 = origin[i][j] + origin[i][j + 1] + origin[i][j + 2] + origin[i][j + 3];
+					// System.out.println(m1);
 					max = Math.max(max, m1);
 				}
-				
-				if(i + 1 < n && j + 1 < m) {
+
+				if (i + 1 < maxleng && j + 1 < maxleng) {
 					int m2 = origin[i][j] + origin[i][j + 1] + origin[i + 1][j] + origin[i + 1][j + 1];
-//					System.out.println(m2);
+					// System.out.println(m2);
 					max = Math.max(max, m2);
 				}
-				
-				if(i + 2 < n && j + 1 < m) {
+
+				if (i + 2 < maxleng && j + 1 < maxleng) {
 					int m3 = origin[i][j] + origin[i + 1][j] + origin[i + 2][j] + origin[i + 2][j + 1];
-//					System.out.println(m3);
+					// System.out.println(m3);
 					max = Math.max(max, m3);
 				}
-				
-				if(i + 2 < n && j + 1 < m) {
+
+				if (i + 2 < maxleng && j + 1 < maxleng) {
 					int m4 = origin[i][j] + origin[i + 1][j] + origin[i + 1][j + 1] + origin[i + 2][j + 1];
-//					System.out.println(m4);
+					// System.out.println(m4);
 					max = Math.max(max, m4);
 				}
-				
-				if(i + 1 < n && j + 2 < m) {
+
+				if (i + 1 < maxleng && j + 2 < maxleng) {
 					int m5 = origin[i][j] + origin[i][j + 1] + origin[i][j + 2] + origin[i + 1][j + 1];
-//					System.out.println(m5);
+					// System.out.println(m5);
 					max = Math.max(max, m5);
 				}
 			}
 		}
 	}
 
-	public static int[][] turnToClockwise(int[][] arr) {
+	public static void turnToClockwise(int[][] arr) {
 		int[][] result = new int[maxleng][maxleng];
 
 		for (int i = 0; i < maxleng; i++) {
@@ -107,28 +105,23 @@ public class Tetromino {
 			}
 		}
 
-//		arr = result;
-		
-		return result;
+		origin = result;
 
-//		 for(int[] a : result) {
-//		 System.out.println(Arrays.toString(a));
-//		 }
+		// for(int[] a : result) {
+		// System.out.println(Arrays.toString(a));
+		// }
 	}
 
-	public static int[][] turnToReverse(int[][] arr) {
+	public static void turnToReverse(int[][] arr) {
 		int[][] result = new int[maxleng][maxleng];
 
 		for (int i = 0; i < maxleng; i++) {
 			for (int j = 0; j < maxleng; j++) {
-				result[i][j] = arr[i][(m - 1) - j];
+				result[i][j] = arr[i][(maxleng - 1) - j];
 			}
 		}
 
-		arr = result;
-		
-		return result;
-
+		origin = result;
 		// for(int[] a : result) {
 		// System.out.println(Arrays.toString(a));
 		// }
