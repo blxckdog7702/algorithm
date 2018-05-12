1e6f4c14234e2fc67ece4a70eca6c259e2f47c88
dohun94 <dohun94@naver.com>
2018-05-12 17:54:19 +0900

    create: c++ array dynamic allocation practice code

---
+++ vsAlgorithm/Algorithm/Algorithm/DynamicAllocation.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int length;
+//
+//	cout << "±æÀÌ´Â?" << endl;
+//
+//	cin >> length;
+//	
+//	int* p;
+//
+//	p = new int[length];
+//
+//	for (int i = 0; i < length; i++) {
+//		cin >> p[i];
+//	}
+//
+//	for (int i = 0; i < length; i++) {
+//		cout << p[i] << " ";
+//	}
+//	
+//	delete[] p;
+//
+//	p = NULL;
+//
+//	return 0;
+//	
+//}
